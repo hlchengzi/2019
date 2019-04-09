bc1a84f13c066dcea625855b90a676907e13a0a1
hlchengzi <34618898+hlchengzi@users.noreply.github.com>
2019-04-09 20:35:22 +0800

    Add files via upload
    hello world项目

---
+++ xcxu_log/initializr-start/src/main/java/com/waylau/spring/boot/blog/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.waylau.spring.boot.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "HELLO WORLD";
+    }
+}
---
+++ xcxu_log/initializr-start/src/test/java/ApplicationTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+    @Test
+    public void main() {
+    }
+}
